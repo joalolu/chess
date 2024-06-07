f8b80f3eb4408cf7d5a040b0fb4fb9c79bcd075e
Joao Silva <joaopauloluiz001@gmail.com>
2024-06-07 17:06:18 -0300

    Position class added

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import board.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		
+		Position p1 = new Position(5, 10);
+	
+	System.out.println("p1 = " + p1);
 	}
 
 }
